312453cebdca2638185b06e0d3af772cbe5e148a
Computational Energetics Laboratory <xuzhiping@gmail.com>
2025-04-23 10:42:36 +0800

    Add files via upload

---
+++ LowTData/Document download/elsevier_xml_download.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import requests
+import time
+import random
+
+
+apikey = '39a5c442bea9f963195796983fef8d21'
+#apikey='YOUR_KEY'
+
+opath="D:/University work/research/Low temperature alloy database/LT_data/6yuebuchong/zhihou/xiazai/xml_els/"
+f=open("Elsevier.txt")
+fout=open(opath+"elsevier_xml_download_log.txt","a")
+doi=[]
+doiname=[]
+for tmp in f.readlines():
+    tmp1=tmp.replace("\n","")
+    tmp1=tmp1.replace(" ","")
+    doi.append(tmp1)
+    doiname.append(tmp1.replace("/","_"))
+f.close()
+
+for i in range(0,len(doi)):
+    try:
+        response=requests.get("https://api.elsevier.com/content/article/doi/"+doi[i]+"?apiKey="+apikey)
+        print(doi[i]+"  status: "+str(response.status_code))
+    except:
+        print(doi[i]+" ERROR")
+        fout.write(doi[i]+" ERROR\n")
+        
+    f=open(opath+doiname[i]+".xml",'w',encoding='utf-8')
+    f.write(response.text)
+    f.close()
+    lag=random.uniform(1,3)
+    time.sleep(lag)      
+    
+fout.close()
